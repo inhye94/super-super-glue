68c205e4d7c6cdcc00300e77a0b63b3c587f0670
DuetoPark <inhye.park.dev@gmail.com>
2024-01-16 18:58:39 +0900

    🎉 프로젝트 셋업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAJbFKyxuwxiIuAUsZDUCXoysLT09iDZXE",
+//   authDomain: "super-super-glue.firebaseapp.com",
+//   databaseURL:
+//     "https://super-super-glue-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "super-super-glue",
+//   storageBucket: "super-super-glue.appspot.com",
+//   messagingSenderId: "267734854567",
+//   appId: "1:267734854567:web:4e76ddc2ca3c2c7d226482",
+//   measurementId: "G-81N7SNK2MB",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">잘 지내고 있니?</div>;
 }
 
 export default App;

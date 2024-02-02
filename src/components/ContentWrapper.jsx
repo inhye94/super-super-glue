00bf4c8356e3beb201b24d33ef5203457967060e
DuetoPark <inhye.park.dev@gmail.com>
2024-02-02 18:10:16 +0900

    💄 ContentWrapper 컴포넌트 추출 (Modules 컴포넌트로부터)

---
+++ src/components/ContentWrapper.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ContentWrapper({ children, title }) {
+  return (
+    <section className="flex flex-col gap-y-[64px] pb-[32px] pt-[24px]">
+      {title && (
+        <h2 className="p-[16px] text-[24px] font-extrabold text-[#333] bg-[#efefef] rounded-md">
+          {title}
+        </h2>
+      )}
+
+      {children}
+    </section>
+  );
+}
--- src/pages/Modules.jsx
+++ src/pages/Modules.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Button from "../components/Button";
 import IconButton from "../components/IconButton";
 import { FiAlertCircle } from "react-icons/fi";
-import LayoutWrapper from "../components/LayoutWrapper";
 import Badge from "../components/Badge";
 import Input from "../components/Input";
 import { FormProvider, useForm } from "react-hook-form";
@@ -15,43 +14,36 @@ import {
   only_kr_validation,
 } from "../utils/validations";
 import FileInput from "../components/FileInput";
+import ContentWrapper from "../components/ContentWrapper";
 
 export default function Modules() {
   return (
-    <LayoutWrapper>
-      <div className="flex flex-col gap-y-[64px] pb-[100px] pt-[24px]">
-        <h2 className="p-[16px] text-[24px] font-extrabold text-[#333] bg-[#efefef] rounded-md">
-          👻 모듈 페이지에용
-        </h2>
-
-        <ButtonSection />
-        <BadgeSection />
-        <FormSection />
-      </div>
-    </LayoutWrapper>
+    <ContentWrapper title="👻 모듈 페이지에용">
+      <ButtonSection />
+      <BadgeSection />
+      <FormSection />
+    </ContentWrapper>
   );
 }
 
 function SectionWrapper({ children, title }) {
   return (
     <section className="flex flex-col gap-y-[24px] p-[16px] bg-[#efefef] rounded-md">
-      <h3 className="p-y-[12px] mb-[24px] text-[24px] font-extrabold text-[#333]">
-        🍟 {title} 🍟
-      </h3>
+      {title && (
+        <h3 className="p-y-[12px] mb-[24px] text-[24px] font-extrabold text-[#333]">
+          🍟 {title} 🍟
+        </h3>
+      )}
 
       {children}
     </section>
   );
 }
 
-function Subtitle({ children }) {
-  return <h4 className="mb-[12px] text-[#aaa]">- {children}</h4>;
-}
-
 function FlexBox({ children, subtitle }) {
   return (
     <div>
-      {subtitle && <Subtitle>{subtitle}</Subtitle>}
+      {subtitle && <h4 className="mb-[12px] text-[#aaa]">- {subtitle}</h4>}
 
       <div className="flex gap-x-[8px] gap-y-[16px] items-center flex-wrap md:flex-nowrap">
         {children}

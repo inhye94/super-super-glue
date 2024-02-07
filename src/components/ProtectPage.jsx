4efe956b892560a58f0256d99f1ca418eea77c88
DuetoPark <inhye.park.dev@gmail.com>
2024-02-08 00:51:23 +0900

    🩹 ProtectPage userInfo 없을 때 Spinner 출력되도록 수정

--- src/components/ProtectPage.jsx
+++ src/components/ProtectPage.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
+import Spinner from "../components/Spinner";
 
 export default function ProtectPage({ children, requiredAdmin }) {
   const { userInfo } = useAuthContext();
 
   if (userInfo === undefined) {
-    return <></>;
+    return <Spinner text="로그인 정보를 확인중입니다!" />;
   } else if (userInfo === null || (requiredAdmin && !userInfo.isAdmin)) {
     return <Navigate to="/" replace />;
   } else {

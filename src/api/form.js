2c48ff2be6f73b534fe57ebaa696adbd386710fb
DuetoPark <inhye.park.dev@gmail.com>
2024-04-23 00:30:56 +0900

    ♻️ axios 적용

--- src/api/form.js
+++ src/api/form.js
@@ -1,12 +1,9 @@
 export function findInputError(errors, name) {
-  const filtered = Object.keys(errors)
+  return Object.keys(errors)
     .filter((key) => key.includes(name))
     .reduce((cur, key) => {
       return Object.assign(cur, { error: errors[key] });
     }, {});
-
-  // console.log(filtered);
-  return filtered;
 }
 
 export const isFormInvalid = (err) => {
--- src/api/uploadFile.js
+++ src/api/uploadFile.js
@@ -1,17 +1,14 @@
+import axios from "axios";
+
 export const uploadFile = async (file) => {
   const formdata = new FormData();
   formdata.append("file", file);
   formdata.append("upload_preset", process.env.REACT_APP_CLOUDINARY_PRESET);
   formdata.append("api_key", process.env.REACT_APP_CLOUDINARY_API_KEY);
 
-  const requestOptions = {
-    method: "POST",
-    body: formdata,
-    redirect: "follow",
-  };
-
-  return fetch(process.env.REACT_APP_CLOUDINARY_URL, requestOptions)
-    .then((response) => response.json())
+  return axios
+    .post(process.env.REACT_APP_CLOUDINARY_URL, formdata)
+    .then((response) => response.data)
     .then((data) => data.url)
     .catch((error) => console.log("error", error));
 };

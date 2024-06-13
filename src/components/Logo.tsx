667b6411d81f3236b16148a48e35930261a59ef0
DuetoPark <inhye.park.dev@gmail.com>
2024-06-13 23:33:21 +0900

    🏷️ typescript 수정 - Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Logo() {
+const Logo: React.FC = () => {
   return (
     <h1>
       <Link
@@ -18,4 +18,6 @@ export default function Logo() {
       </Link>
     </h1>
   );
-}
+};
+
+export default Logo;
